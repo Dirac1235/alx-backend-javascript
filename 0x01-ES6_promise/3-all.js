31967ba6720f25af7ebb0339216e34766470a689
Webi Muleta <webimuleta01@gmail.com>
2024-06-04 22:48:37 +0300

    lint changes

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,9 @@
 import { createUser, uploadPhoto } from './utils';
+
 function handleProfileSignup() {
   Promise.all([createUser(), uploadPhoto()])
-  .then((values) => {
-    console.log(values[1].body, values[0].firstName, values[0].lastName)
-  })
+    .then((values) => {
+      console.log(values[1].body, values[0].firstName, values[0].lastName);
+    });
 }
 handleProfileSignup();
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,11 +3,9 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise
-  .allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-  .then((result) => result.map((data) => {
-    return {
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+    .then((result) => result.map((data) => ({
       status: data.status,
       value: data.status === 'fulfilled' ? data.value : String(data.reason),
-    };
-  }));
+    })));
 }
